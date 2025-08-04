ceb87f30b7768c31ddc6541e886bdaa9fea99603
ryotakonishi <ryota.konishi@icloud.com>
2025-08-04 16:03:17 +0900

    [add]ゲージ管理処理追加

--- Assets/Konishi/Gauge.cs
+++ Assets/Konishi/Gauge.cs
@@ -3,23 +3,28 @@ using UnityEngine.UI;
 
 public class Gauge : MonoBehaviour
 {
-    public int MaxGauge;
-    public int MinGauge;
-    public int SobaValue; 
-    public int ToysValue;
+    public float MaxGauge = 100;
+    public float MinGauge = 0;
+    public float SobaValue = 10; 
+    public float ToysValue = 10;
 
-    public int _gaugeValue;
+    public float GaugeValue = 50;
 
     private Image _image;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _image = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetGauge()
+    {
+        _image.fillAmount = GaugeValue / MaxGauge;
     }
 }
