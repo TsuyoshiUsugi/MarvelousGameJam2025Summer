69d4d6c12f16a12d3dadd3aab5d2dfe3fce2b3a4
junyoko1 <junyoko1@outlook.jp>
2025-08-04 16:20:14 +0900

    キャンバスと　操作説明をつけたり減らしたりするボタンの追加

---
+++ Assets/yokosyouji/Button.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private Image _manualImage;
+    bool _imageBool=false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Manual()
+    {
+        _imageBool =! _imageBool;
+        _manualImage.gameObject.SetActive(_imageBool);
+    }
+
+}
