79c443846aebfed637b3b48902428ffca9cd9991
ryotakonishi <ryota.konishi@icloud.com>
2025-08-04 12:13:33 +0900

    [add]コルーチン起動

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -32,7 +32,7 @@ public class Player : MonoBehaviour
         if (other.gameObject.CompareTag("Obstacle"))
         {
             _canMove = false;
-
+            Damaged();
         }
     }
     private void Move()
