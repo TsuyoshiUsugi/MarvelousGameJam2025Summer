0b6d69aa5a685c6d9303ac0d0282105b5f475f3a
ryotakonishi <ryota.konishi@icloud.com>
2025-08-04 17:17:47 +0900

    [add]回転するように

--- Assets/yokosyouji/Item.cs
+++ Assets/yokosyouji/Item.cs
@@ -1,12 +1,18 @@
 using DG.Tweening;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
     [SerializeField] private float _speed = 0.3f;
+    [SerializeField] float _rotationTime;
     void Update()
     {
         transform.position -= new Vector3(_speed * Time.deltaTime,0);
+        if (gameObject.CompareTag("Soba"))
+        {
+            this.transform.DORotate(new Vector3(0, 180, 0), _rotationTime).
+                           SetLoops(-1).
+                           SetRelative(true);
+        }
     }
 }
