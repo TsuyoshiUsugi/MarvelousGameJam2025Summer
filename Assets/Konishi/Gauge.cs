7e2bb21e62e6236551c54c356e48b78c9ff84b80
ryotakonishi <ryota.konishi@icloud.com>
2025-08-05 12:37:05 +0900

    [add]ゲージが空腹か満腹になった時にシーン遷移 ゲージが減り続ける

--- Assets/Konishi/Gauge.cs
+++ Assets/Konishi/Gauge.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Gauge : MonoBehaviour
 {
+    [SerializeField,Header("–ˆ•bŒ¸‚é’l")] float ChangeGausePerSecond;
+
+
     public float MaxGauge = 100;
     public float MinGauge = 0;
     public float SobaValue = 10; 
     public float ToysValue = 10;
 
-    public float GaugeValue = 50;
+    [SerializeField] private float _gaugeValue = 50;
+    public float GaugeValue { set { SetGauge(); _gaugeValue = value; } get => _gaugeValue; }
 
     private Image _image;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,7 +25,11 @@ public class Gauge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GaugeValue >= MaxGauge || GaugeValue <= MinGauge)
+        {
+            SceneManager.LoadScene("result");
+        }
+        GaugeValue -= ChangeGausePerSecond * Time.deltaTime;
     }
 
     public void SetGauge()
