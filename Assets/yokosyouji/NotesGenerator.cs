402155ef40ed0ee7c74aae9027fd31049e74773a
junyoko1 <junyoko1@outlook.jp>
2025-08-05 11:59:14 +0900

    [fix,add]ジェネレーター

--- Assets/yokosyouji/NotesGenerator.cs
+++ Assets/yokosyouji/NotesGenerator.cs
@@ -1,45 +1,63 @@
 using System;
-using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class NotesGenerator : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    [SerializeField] GameObject _soba;
-    //[SerializeField] GameObject _toys;
-    //[SerializeField] GameObject _obstacle;
+    [SerializeField] GenerateDeta[] _item;
     //¶¬ˆÊ’u
     [SerializeField] GameObject[] _points;
 
     [SerializeField] float _generateTime = 3f;
-    
-    
+    System.Random _random = new System.Random();
+
     float _count;
-    
+    int _itemAllWeight;
+
     void Start()
     {
-       
+
+        foreach (GenerateDeta gd in _item)
+        {
+            _itemAllWeight += gd.Weight;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         _count += Time.deltaTime;
-        if (_count > _generateTime)
-        {
-            System.Random _random = new System.Random();
-            int num =_random.Next(0,3);
-            Instantiate(_soba, _points[num].transform.position,Quaternion.identity);
 
-            Debug.Log(num);
+        if (_count >= _generateTime)
+        {
+
+            int spawanpoint = _random.Next(0, _points.Length);
+
+            int randomint = _random.Next(1, _itemAllWeight + 1);
+            Debug.Log(randomint);
+            foreach (var gd in _item)
+            {
+                randomint -= gd.Weight;
+                if (randomint <= 0)
+                {
+                    Debug.Log(gd.GameObject);
+                    Instantiate(gd.GameObject).transform.position = _points[spawanpoint].transform.position;
+                    break;
+                }
+            }
             _count = 0;
         }
+
 
     }
 
 
-
-
+}
+[Serializable]
+public class GenerateDeta
+{
+    public GameObject GameObject;
+    public int Weight;
 }
